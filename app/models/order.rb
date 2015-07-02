4fe18a49b27e1ca8326df3594dd3d7b7b83b3156
Fernando <krajka.bartosz@gmail.com>
2015-07-02 15:54:49 +0200

    POSSIBLE_STATES is now a constant

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   validates :content, :user, presence: true
-  possible_states = %w(active finalized ordered delivered)
-  validates :state, :inclusion => {:in => possible_states}
+  POSSIBLE_STATES = %w(active finalized ordered delivered)
+  validates :state, :inclusion => {:in => POSSIBLE_STATES}
 end
